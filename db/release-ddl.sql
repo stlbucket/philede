4fc8b9b2d7feda22e751cabdca74a635aa2edf2f
stlbucket <stlbucket@gmail.com>
2018-10-05 21:05:43 -0700

    release ddl up and down

--- db/release-ddl.sql
+++ db/release-ddl.sql
@@ -1,16 +1,141 @@
 ------------------------------------------------
 -- release_ddl_up
 ------------------------------------------------
-create or replace function pde.release_ddl_up(release pde.release)
-returns text as $$
-  select 'ddl_up'::text;
-$$ language sql stable;
+CREATE OR REPLACE FUNCTION pde.release_ddl_up(
+  release pde.release
+)
+  RETURNS text AS
+$BODY$
+DECLARE
+  _ddl_up text;
+  _minor pde.minor;
+  _patch pde.patch;
+  _artifact pde.artifact;
+  _artifact_type pde.artifact_type;
+BEGIN
+  _ddl_up := '-- phile-de generated script
+  release:' || release.number || '  
+  ';
+  
+  for _minor in
+    select * from pde.minor where release_id = release.id
+  loop
+    _ddl_up := _ddl_up || '
+===============================================================================
+    minor patch set: ' || _minor.name || ' - ' || _minor.number || '  
+===============================================================================
+';
+
+    for _patch in
+      select * from pde.patch where minor_id = _minor.id
+    loop
+      select * into _artifact from pde.artifact where id = _patch.artifact_id;
+      select * into _artifact_type from pde.artifact_type where id = _artifact.artifact_type_id;
+
+      _ddl_up := _ddl_up || '
+  -------------------------------------------------------------------------------
+      patch:             ' || _patch.number || '
+      artifact type:     ' || _artifact_type.name || '
+      artifact:          ' || _artifact.name || '
+  -------------------------------------------------------------------------------
+  ';
+      _ddl_up := _ddl_up || '
+
+    ' || _patch.ddl_up || '
+
+      ';
+
+      _ddl_up := _ddl_up || '
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      end patch: ' || _patch.number || '
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  ';
+    end loop;
+
+    _ddl_up := _ddl_up || '
+*******************************************************************************
+    end minor patch set: ' || _minor.name || ' - ' || _minor.number || '  
+*******************************************************************************
+';
+  end loop;
+
+  return _ddl_up;
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE STRICT SECURITY DEFINER
+  COST 100;
 
 ------------------------------------------------
 -- release_ddl_down
 ------------------------------------------------
-create or replace function pde.release_ddl_down(release pde.release)
-returns text as $$
-  select 'ddl_down'::text;
-$$ language sql stable;
+CREATE OR REPLACE FUNCTION pde.release_ddl_down(
+  release pde.release
+)
+  RETURNS text AS
+$BODY$
+DECLARE
+  _ddl_down text;
+  _minor pde.minor;
+  _patch pde.patch;
+  _artifact pde.artifact;
+  _artifact_type pde.artifact_type;
+BEGIN
+  _ddl_down := '-- phile-de generated script
+  release:' || release.number || '  
+  ';
+  
+  for _minor in
+    select * from pde.minor where release_id = release.id order by revision desc
+  loop
+    _ddl_down := _ddl_down || '
+===============================================================================
+    minor patch set: ' || _minor.name || ' - ' || _minor.number || '  
+===============================================================================
+';
 
+    for _patch in
+      select * from pde.patch where minor_id = _minor.id order by revision desc
+    loop
+      select * into _artifact from pde.artifact where id = _patch.artifact_id;
+      select * into _artifact_type from pde.artifact_type where id = _artifact.artifact_type_id;
+
+      _ddl_down := _ddl_down || '
+  -------------------------------------------------------------------------------
+      patch:             ' || _patch.number || '
+      artifact type:     ' || _artifact_type.name || '
+      artifact:          ' || _artifact.name || '
+  -------------------------------------------------------------------------------
+  ';
+      _ddl_down := _ddl_down || '
+
+    ' || _patch.ddl_up || '
+
+      ';
+
+      _ddl_down := _ddl_down || '
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      end patch: ' || _patch.number || '
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  ';
+    end loop;
+
+    _ddl_down := _ddl_down || '
+*******************************************************************************
+    end minor patch set: ' || _minor.name || ' - ' || _minor.number || '  
+*******************************************************************************
+';
+  end loop;
+
+  return _ddl_down;
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE STRICT SECURITY DEFINER
+  COST 100;
+
+
+
+select pde.release_ddl_up(release)
+from pde.release release where status = 'Development'
+;
--- web/src/components/Release/ReleaseDetail.vue
+++ web/src/components/Release/ReleaseDetail.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
-    <h1>Release - {{ releaseNumber }}</h1>
+    <h1>Release - {{ release.number }}</h1>
     <v-btn @click="done">Done</v-btn>
+    <v-textarea
+      :value="release.ddlUp"
+      auto-grow
+      readonly
+    ></v-textarea>
   </div>
 </template>
 
 <script>
+import releasePatchTree from '../../gql/query/releasePatchTree.gql'
 
 export default {
   name: "ReleaseDetail",
@@ -17,18 +23,33 @@ export default {
     }
   },
   computed: {
-    releaseNumber () {
-      return this.release ? this.release.number : 'N/A'
+    focusReleaseId () {      
+      return this.$store.state.focusReleaseId
     }
   },
-  props: {
-    // id: {
-    //   type: String,
-    //   required: true
-    // }
+  apollo: {
+    init: {
+      query: releasePatchTree,
+      variables () {
+        return {
+          id: this.focusReleaseId
+        }
+      },
+      skip () {
+        return this.focusReleaseId === ''
+      },
+      fetchPolicy: 'network-only',
+      update (result) {
+        this.release = result.release
+        console.log('this.release', this.release.ddlUp)
+        // this.allArtifactTypes = result.allArtifactTypes.nodes
+      }
+    }
+
   },
   data () {
     return {
+      release: {}
     }
   },
 }
